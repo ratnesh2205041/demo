35d081b977ee89854ed72e964efc3a13763a7c15
ratnesh2205041 <157671445+ratnesh2205041@users.noreply.github.com>
2024-02-21 17:27:39 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,8 @@
+class main{
+    public static void main (string[]args){
+        int first =10;
+        int second = 20
+        int sum =first + second
+        System.out.println(first+"+"second+"+"+sum)
+    }
+}
